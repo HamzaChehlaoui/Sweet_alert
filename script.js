3f37717dd9602e6d1ca140baea4123b7b543788d
Hamza <hamzacoude@gmail.com>
2024-12-15 01:20:50 +0100

    add interface sweet danger

--- script.js
+++ script.js
@@ -3,26 +3,24 @@ let btn_info=document.querySelector(".btn-info");
 let contore=document.querySelector(".contore");
 let del_hid_info=document.querySelector(".del-hid-info");
 
-  del_hid_info.onclick=function(){
+    // function sweet-info
+
+del_hid_info.onclick=function(){
     info.classList.remove("hidden");
     let i=5;
-btn_info.onclick=function(){
+    btn_info.onclick=function(){
     info.classList.add("hidden");
-}
-
-setTimeout( function(){
-
+    }
+  setTimeout( function(){
     info.classList.add("hidden");
 
-},6000);
-
-let intervalID = setInterval(function() {
+  },6000);
+  let intervalID = setInterval(function() {
     contore.innerHTML=`${i}`;
     i--;
   }, 1000);
-  
   setTimeout(function() {
     clearInterval(intervalID);
   }, 5000);
   contore.innerHTML=`6`;
-  }
+}
